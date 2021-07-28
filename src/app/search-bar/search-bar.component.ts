864260950466972d60f1cde5ef561f4c5aaa5b1c
omogbemeh <onosithena@gmail.com>
2021-07-28 17:29:12 +0100

    Fixed the sorm input

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -8,7 +8,8 @@ import { Component, Output, EventEmitter } from '@angular/core';
 export class SearchBarComponent {
   @Output() submitted = new EventEmitter();
   term = '';
-  onSubmit(e: string, inp: any) {
+  onSubmit(e: string, inp: any, event: Event) {
+    event.preventDefault();
     inp.value = '';
     this.submitted.emit(this.term);
     this.term = '';
